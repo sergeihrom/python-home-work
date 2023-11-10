96979b074ff427024aebc67995feb673bc8760e9
sergeihrom <sergei.hromov@inbox.ru>
2023-11-10 16:16:11 +0600

    Create week2.py

---
+++ dz 06.11.23/week2.py
@@ -0,0 +1,8 @@
+print("Mondey",  end=", ")
+print("Tuesday", end=", ")
+print("Wednesday", end=", ")
+print("Thursday", end=", ")
+print("Friday", end=", ")
+print("Saturday", end=", ")
+print("Sunday", end=", ")
+#команда print выводит текст на экран, а параметр end ставит запятую между пееремменными.
