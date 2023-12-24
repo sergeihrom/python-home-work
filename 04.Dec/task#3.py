d8345ce1a7a2e80352ad1cfa1d51f5b642c3c7f7
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-12-24 20:23:49 +0600

    Проверено дз от 04.12
    Комментарии к коду

--- 04.Dec/task#3.py
+++ 04.Dec/task#3.py
@@ -1,6 +1,7 @@
 
 word = input("Введите слово: ")
 
+#VN: тут важно сделать проверку длины слова, иначе 'me' сокращается как 'me-me'
 abbreviation = word[:2] + "-" + word[-2:]
 
 print(f"Слово: {word} сокращается как: {abbreviation}")
